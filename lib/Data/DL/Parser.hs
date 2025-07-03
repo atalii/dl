d2dcc4e443668d0b2e28890db77e7db21f52b302
Tali Auster <me@tali.network>
2025-07-03 16:17:34 -0600

    chore: fix license headers

--- lib/Data/DL/Parser.hs
+++ lib/Data/DL/Parser.hs
@@ -1,9 +1,8 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE StandaloneDeriving #-}
-
 -- This Source Code Form is subject to the terms of the Mozilla Public License,
 -- v. 2.0. If a copy of the MPL was not distributed with this file, You can
 -- obtain one at https://mozilla.org/MPL/2.0/.
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Data.DL.Parser
   ( Antecedent,
